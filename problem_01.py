3dcd4c156b3c4da144e4a1c085ea35a471a44b9a
Monster-Coder <>
2025-02-11 18:12:03 +0800

    Last Sync: 2025-02-11 10:12:03 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -5,6 +5,8 @@
 
 def main():
     print("Simple Calculator Program")
+    num1 = int(input("Enter 1st number: "))
+    num2 = int(input("Enter 2nd number: "))
 
 if __name__ == "__main__":
     main()
