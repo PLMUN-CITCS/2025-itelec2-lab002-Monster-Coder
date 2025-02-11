9c5fd2cefa73408fc9e2f1b325c39415f84d5be9
Monster-Coder <>
2025-02-11 18:25:45 +0800

    Last Sync: 2025-02-11 10:25:45 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -7,7 +7,6 @@ def main():
     print("Simple Calculator Program")
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
-    
     print(f"The sum is {num1 + num2}")
     print(f"The difference is {num1 - num2}")
     print(f"The product is {num1 * num2}")
